3307007ef8e2332ef337e6a4604a41993d7c78bd
DevDeismon <fco.ismaelmartingonzalez@gmail.com>
2022-01-21 20:15:47 +0100

    Init final proyect

---
+++ src/main/java/com/cide/larios/LariosApplication.java
@@ -0,0 +1,13 @@
+package com.cide.larios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cide/larios/LariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cide.larios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
